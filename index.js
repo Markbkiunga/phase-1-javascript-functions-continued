9173c26a6dcfb96d4f474a73e51f8309965caa88
Mark Brian <markbkiunga2017@gmail.com>
2024-06-26 20:03:30 +0300

    Completes Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  console.log(`This Saturday, I want to ${activity}!`);
+  return `This Saturday, I want to ${activity}!`;
+}
+// saturdayFun();
+function mondayWork(activity = "go to the office") {
+  console.log(`This Monday, I will ${activity}.`);
+  return `This Monday, I will ${activity}.`;
+}
+// mondayWork();
+let wrapAdjective = function (wrappingFlair = "*") {
+  function statement(adjective = "special") {
+    return `You are ${wrappingFlair}${adjective}${wrappingFlair}!`;
+  }
+  return statement;
+};
+wrapAdjective()();
